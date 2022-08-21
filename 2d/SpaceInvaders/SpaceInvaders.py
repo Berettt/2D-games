b337104be9d65891fb28f6ae423985d63c6e44ff
Michał <97108402+Berettt@users.noreply.github.com>
2022-08-21 09:50:29 +0200

    shooting and movement

--- 2d/SpaceInvaders/SpaceInvaders.py
+++ 2d/SpaceInvaders/SpaceInvaders.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 class SpaceInvader:
 
@@ -7,7 +7,7 @@ class SpaceInvader:
         pygame.init()
 
         self.pyclock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((850,600))
+        self.screen = pygame.display.set_mode((850,800))
         pygame.display.set_caption('Space Invaders')
 
 
@@ -18,14 +18,23 @@ class SpaceInvader:
         self.player = pygame.image.load('stuff/player.png').convert_alpha()
         self.playerRECTANGLE = self.player.get_rect()
 
-    def main_loop(self):
+    def mobs(self):
+        self.mob = pygame.image.load('stuff/ufo.png').convert_alpha()
+        self.mobRECTANGLE = self.mob.get_rect()
+        self.randomx = random.randint(10,840)
 
-        self.playerRECTANGLE.y = 500
+    def main_loop(self):
+        self.fire = False
+        MOBMOVEMENT = 2.5
+
+        self.playerRECTANGLE.y = 700
         self.playerRECTANGLE.x = 400 
+
+        self.shotRECTANGLE.y = 700
 
         XuniformlyXplayer= 0
         YuniformlyYplayer= 0
-        
+
         while True:
 
             for ewent in pygame.event.get():
@@ -33,12 +42,15 @@ class SpaceInvader:
                 if ewent.type == pygame.QUIT:
                     exit()
                 if ewent.type == pygame.KEYDOWN:
+
+                    if ewent.key == pygame.K_w and ewent.key == pygame.K_a:
+                        print('test')
                     
                     if ewent.key == pygame.K_w:
-                        pass
+                        YuniformlyYplayer = -2 
 
                     if ewent.key == pygame.K_s:
-                        pass     
+                        YuniformlyYplayer = 2      
 
                     if ewent.key == pygame.K_a:
                         XuniformlyXplayer = -2 
@@ -46,23 +58,49 @@ class SpaceInvader:
                     if ewent.key == pygame.K_d: 
                         XuniformlyXplayer = 2
 
+                    if ewent.key == pygame.K_SPACE:
+                        self.fire_bullet()
+
                 if ewent.type == pygame.KEYUP:
                     XuniformlyXplayer = 0
+                    YuniformlyYplayer = 0
 
             self.screen.blit(self.background,(0,0)) #background
             self.screen.blit(self.player,(self.playerRECTANGLE.x,self.playerRECTANGLE.y)) #player
+            self.screen.blit(self.mob,(self.randomx,self.mobRECTANGLE.y)) #mob
+            self.screen.blit(self.shot,(self.playerRECTANGLE.x,self.shotRECTANGLE.y))
 
             self.playerRECTANGLE.x += XuniformlyXplayer #movement x
             self.playerRECTANGLE.y += YuniformlyYplayer #movement y
+            self.mobRECTANGLE.y += MOBMOVEMENT
+            self.multiple_bullets()
+            self.shotRECTANGLE.y -= self.bulletchange
+
 
             pygame.display.update()
             self.pyclock.tick(60) 
-    
-    def mobs(self):
-        pass
 
+   
     def bullet(self):
-        pass
+        self.shot = pygame.image.load('stuff/bullet.png')
+        self.shotRECTANGLE = self.shot.get_rect()
+        self.bulletchange = 0
+    
+    def fire_bullet(self):
+        self.bulletchange = 15
+
+    def multiple_bullets(self):
+         if self.shotRECTANGLE.y <=0:
+            self.shotRECTANGLE.y = 700
+            self.fire = True
+
+         if self.fire == True:
+            self.shotRECTANGLE.y = self.playerRECTANGLE.y
+            self.shotRECTANGLE.x = self.playerRECTANGLE.x
+            self.screen.blit(self.shot,(self.shotRECTANGLE.x,self.shotRECTANGLE.y))
+            self.shotRECTANGLE.y -= self.bulletchange
+            self.fire = False
+
 
     def config(self):
         pass
@@ -73,6 +111,8 @@ def main():
     game = SpaceInvader()
     game.init()
     game.main()
+    game.mobs()
+    game.bullet()
     game.main_loop()
 
 if __name__ == '__main__':
